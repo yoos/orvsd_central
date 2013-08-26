ef7271cba4f909c1b39a027cb7ea4e9220f69a2d
Soo-Hyun Yoo <yoos117@gmail.com>
2013-08-26 12:30:21 -0700

    Don't import celery from tasks.

--- orvsd_central/views.py
+++ orvsd_central/views.py
@@ -12,7 +12,6 @@ from sqlalchemy import func, and_
 from sqlalchemy.sql.expression import desc
 from models import (District, School, Site, SiteDetail,
                     Course, CourseDetail, User)
-from tasks import celery
 import json
 import re
 import subprocess
